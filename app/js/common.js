0888782c510dbea66805bd69f2ec8315be782f37
Viktoria <zaharova181114@gmail.com>
2021-07-04 01:09:57 +0300

    media

--- app/js/common.js
+++ app/js/common.js
@@ -11,5 +11,42 @@ $(".video-js").on("click", function(){
 $('.instruction-work-slider').slick({
     slidesToShow: 3,
     nextArrow: '<button type="button" class="slick-next"></button>',
-    prevArrow: '<button type="button" class="slick-prev"></button>'
+    prevArrow: '<button type="button" class="slick-prev"></button>',
+    responsive: [
+        {
+            breakpoint: 992,
+            settings: {
+                slidesToShow: 2,
+            }
+        },
+        {
+            breakpoint: 576,
+            settings: {
+                slidesToShow: 1,
+            }
+        }
+    ]
+});
+
+
+// slick active
+$(window).on('load resize', function() {
+    if ($(window).width() < 992) {
+        $('.reviews-slider:not(.slick-initialized)').slick({
+            slidesToShow: 1,
+            nextArrow: '<button type="button" class="slick-next"></button>',
+            prevArrow: '<button type="button" class="slick-prev"></button>',
+        });
+    } else {
+        $(".reviews-slider.slick-initialized").slick("unslick");
+    }
+});
+// slick active
+
+$('.btn-burger').on('click', function () {
+   $('.menu').fadeToggle();
+});
+
+$('.btn-close').on('click', function () {
+    $('.menu').fadeOut();
 });
